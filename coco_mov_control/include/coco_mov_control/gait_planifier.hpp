bf130c44a580521c79fb2c9bdf36cec739c886fb
JustoDario <justodariovalverde@gmail.com>
2025-07-21 18:22:46 +0200

    left to adjust stability

--- coco_mov_control/include/coco_mov_control/gait_planifier.hpp
+++ coco_mov_control/include/coco_mov_control/gait_planifier.hpp
@@ -61,7 +61,7 @@ private:
   const float max_vel_ = 0.4;
   const float std_vel_ = 0.25;
   const float min_vel_ = 0.15;
-  const float std_real_step_time = 1.2;//secs
+  const float std_real_step_time = 1;//secs
 
   static const int STANDBY = 0;
   static const int WALKING = 1;
